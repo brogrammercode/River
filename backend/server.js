7832e7d3b39d74af61175225ecac3daa89cbd5b5
Harsh <harsh.bhumihar7@gmail.com>
2025-09-05 23:51:56 +0530

    versel deploy bug fix #2

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,5 @@
 import app from "./src/app.js";
-import env from "./src/config/env.js";
 import { connectMongo } from "./src/config/mongo.js";
-import logger from "./src/utils/logger.js";
 
-const { PORT } = env;
-
-connectMongo().then(() => {});
+connectMongo();
 export default app;
