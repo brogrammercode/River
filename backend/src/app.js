343aaa06e560d0afb6e6065349bf9e622e6e42d6
Harsh <harsh.bhumihar7@gmail.com>
2025-09-06 01:23:50 +0530

    versel deploy bug fix #11

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,17 +5,26 @@ import { initLogger } from "./utils/logger.js";
 import routes from "./routes/index.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
-app.get("/", (req, res) => res.send("Server is running!"));
 app.use(
   cors({
     origin: "https://river-ruddy.vercel.app",
     credentials: true,
   })
 );
+app.use(express.json());
+app.use(cookieParser());
 app.use(initLogger);
 app.use(errorHandler);
+app.get("/", (req, res) => res.send("Server is running!"));
+app.get("/db-test", async (req, res) => {
+  try {
+    await mongoose.connection.db.listCollections().toArray();
+    res.send("MongoDB is connected!");
+  } catch (err) {
+    res.status(500).send("MongoDB connection failed: " + err.message);
+  }
+});
+
 
 app.use("/api", routes);
 
