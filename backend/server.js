ae4e180ee5e2892da007f18a6011f5e6b3d71d26
Harsh <harsh.bhumihar7@gmail.com>
2025-09-06 01:18:28 +0530

    versel deploy bug fix #10

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,10 @@
-import mongoose from "mongoose";
-
 import app from "./src/app.js";
 import env from "./src/config/env.js";
 import connectMongo from "./src/config/mongo.js";
 import logger from "./src/utils/logger.js";
 const { NODE_ENV, PORT } = env;
 
-// await connectMongo();
-mongoose
-  .connect(
-    "mongodb+srv://harshsharma55115:n70eWb1EImf7EsOT@tuto-1.gqteplu.mongodb.net/river_db?retryWrites=true&w=majority&appName=tuto-1"
-  )
-  .then(() => console.log("MONGO_CONNECTED"))
-  .catch((err) => console.error("MONGO_CONNECTION_ERROR", err));
-
+await connectMongo();
 if (NODE_ENV === "development") {
   app.listen(PORT, () => {
     logger.info(`PORT_CONNECTED: ${PORT}`);
